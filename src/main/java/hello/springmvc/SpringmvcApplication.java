f8472e81991c7f23816ff207a6f69eac37e59dd3
DaeHyeonHong <fholng@outlook.com>
2023-07-30 20:55:23 +0900

    ProjectInit

--- src/main/java/hello/springmvc/SpringmvcApplication.java
+++ src/main/java/hello/springmvc/SpringmvcApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringmvcApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SpringmvcApplication.class, args);
     }
-
 }
